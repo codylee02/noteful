135326e1e73a840388f72bc833b411e3fb7801e7
Cody Lee <codylee02@gmail.com>
2019-08-22 13:47:31 -0600

    removed console.logs and formatted document

--- src/Notes/Notes.js
+++ src/Notes/Notes.js
@@ -1,51 +1,48 @@
-import React from 'react';
-import './Notes.css';
-import {Link} from 'react-router-dom';
-import dummyStore from '../dummy-store.js';
-
+import React from "react";
+import "./Notes.css";
+import { Link } from "react-router-dom";
+import dummyStore from "../dummy-store.js";
 
 export default class Notes extends React.Component {
-    render() {
+  render() {
+    const foundCards = this.props.match
+      ? dummyStore.notes.filter(
+          note => note.folderId === this.props.match.params.folderId
+        )
+      : null;
 
-        console.log('dummystoresays',dummyStore);
-        const foundCards = this.props.match ? (dummyStore.notes.filter(note => 
-            note.folderId === this.props.match.params.folderId)) : null
-            
-        
-        const folderId = this.props.match 
-        ? foundCards.map(card => {
-            return (<li key={card.id}><Link to={`/note/${card.id}`}><h2>{card.name}</h2></Link>
-            <p>Date Modified: {card.modified}</p>
-            <button>Delete Note</button></li>)
-        }) 
-        : this
-        .props
-        .notes
-        .map(card => {
-         return(   <li key={card.id}>
-             
-                <Link to={`/note/${card.id}`}><h2>{card.name}</h2></Link>
-                <p>Date Modified: {card.modified}</p>
-                <button>Delete Note</button>
-                
-            </li>)
-
+    const folderId = this.props.match
+      ? foundCards.map(card => {
+          return (
+            <li key={card.id}>
+              <Link to={`/note/${card.id}`}>
+                <h2>{card.name}</h2>
+              </Link>
+              <button className="delete-button">Delete Note</button>
+              <p>Date Modified: {card.modified}</p>
+            </li>
+          );
         })
+      : this.props.notes.map(card => {
+          return (
+            <li key={card.id}>
+              <Link to={`/note/${card.id}`}>
+                <h2>{card.name}</h2>
+              </Link>
+              <button className="delete-button">Delete Note</button>
+              <p>Date Modified: {card.modified}</p>
+            </li>
+          );
+        });
 
-        console.log(folderId)
-        console.log('currentprops', this.props)
-        
-
-        
-            
 
     return (
-        <section className="note-list">
-        <ul >
-            {folderId}
-            <button>Add Note</button>
+      <section className="note-list">
+        <ul>
+          {folderId}
+          <button>Add Note</button>
         </ul>
-        </section>
-    )
-    }
+      </section>
+    );
+  }
 }
