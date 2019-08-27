53ed9a5e7b53bca9f7a2674357800536238f2c39
Cody Lee <codylee02@gmail.com>
2019-08-27 13:26:18 -0600

    formatted & removed unnecessary comments

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,82 +1,61 @@
 import React from "react";
-import ValidationError from '../ValidationError/ValidationError'
-import NotefulContext from '../NotefulContext';
-//Add error handling
-//don't accept empty strings.
+import ValidationError from "../ValidationError/ValidationError";
+import NotefulContext from "../NotefulContext";
 
 
 export default class AddFolder extends React.Component {
-    static contextType = NotefulContext;
-    constructor(props) {
-        super(props);
-        this.folderInput = React.createRef();
-        this.state = {
-            newFolder: ''
-            
-        }
+  static contextType = NotefulContext;
+  constructor(props) {
+    super(props);
+    this.folderInput = React.createRef();
+    this.state = {
+      newFolder: ""
+    };
+  }
+  updateFolder(folder) {
+    this.setState({ newFolder: folder });
+  }
 
-    }
-updateFolder(folder) {
-    this.setState({newFolder: folder})
-}
-
-validateFolderName() {
+  validateFolderName() {
     const newFolderName = this.state.newFolder.trim();
-    const existingFoldersArr = this.context.noteStore.folders
-    
-    
+    const existingFoldersArr = this.context.noteStore.folders;
+
     if (newFolderName.length === 0) {
-        return `Folder's title cannot be blank.`;
+      return `Folder's title cannot be blank.`;
     }
 
     for (let i = 0; i < existingFoldersArr.length; i++) {
-        if (existingFoldersArr[i].name === newFolderName) {
-            return `Folder's name already exists. Please choose another one`
-        }
-
+      if (existingFoldersArr[i].name === newFolderName) {
+        return `Folder's name already exists. Please choose another one`;
+      }
     }
-    
-}
-
-
+  }
 
   handleSubmit = event => {
     event.preventDefault();
     const newFolder = {
-        id: this.state.newFolder,
-        name: this.state.newFolder
-    }
+      id: this.state.newFolder,
+      name: this.state.newFolder
+    };
     fetch(`http://localhost:9090/folders/`, {
-        method: 'POST',
-        body: JSON.stringify(newFolder),
-        headers: {
-            'content-type': 'application/json',
-        }
+      method: "POST",
+      body: JSON.stringify(newFolder),
+      headers: {
+        "content-type": "application/json"
+      }
     })
-    .then(res => {
-        if(!res.ok) {
-            return res.json().then(error => {
-                throw error
-            })
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(error => {
+            throw error;
+          });
         }
-        return res.json()
-    })
-    .then(data => {
-        console.log(data)
-        this.context.addFolder(data)
+        return res.json();
+      })
+      .then(data => {
+        this.context.addFolder(data);
         this.props.history.goBack();
-    })
-
-
-
-
-
-
-    //check that the submission is not an empty string
-    //post the newFolder to the server
-    //update noteStore state on App
-    //GoHome
-
+      });
   };
 
   handleClickCancel = () => {
@@ -87,9 +66,6 @@ validateFolderName() {
       <>
         <h2>Add a new Folder</h2>
         <form className="AddFolder__form" onSubmit={this.handleSubmit}>
-          {/* <div className="AddFolder__error" role='alert'>
-                    {error && <p>{error.message}</p>} 
-                </div>*/}
           <label htmlFor="folder">Folder Name: </label>
           <input
             type="text"
@@ -99,18 +75,16 @@ validateFolderName() {
             placeholder="ex: New Folder 123"
             onChange={e => this.updateFolder(e.target.value)}
             required
-          />
-          {' '}
+          />{" "}
           <button type="button" onClick={this.handleClickCancel}>
             Cancel
+          </button>{" "}
+          <button type="submit" disabled={this.validateFolderName()}>
+            Save
           </button>
-          {' '}
-          <button type="submit"
-          disabled={this.validateFolderName()}>Save</button>
         </form>
         <ValidationError message={this.validateFolderName()} />
       </>
     );
   }
 }
-
